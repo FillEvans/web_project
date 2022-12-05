876f6c80469af4685e07c25cb3133a27ca15c351
Fill Evans <filipp.glinchuk@gmail.com>
2022-12-05 20:49:03 +0300

    added everything

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template("main.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/enroll')
+def enroll():
+    return render_template("enroll.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
